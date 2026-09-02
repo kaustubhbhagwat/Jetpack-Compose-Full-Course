49255b6764c08d3297ca5e3e781d8ac612069776
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2026-09-02 19:22:36 +0530

    Lazy Column Sample

---
+++ app/src/main/java/com/jetpackcomposefullcourse/layout_composables/LazyColumnAndRow.kt
@@ -0,0 +1,47 @@
+package com.jetpackcomposefullcourse.layout_composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+// Lazy Column
+
+@Composable
+fun LazyColumnExample() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Lazy Column")
+        LazyColumn() {
+            items(100) { index ->
+                Text(
+                    "This is item no $index",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .background(Color.Magenta)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun LazyColumnExamplePreview() {
+    LazyColumnExample()
+}
